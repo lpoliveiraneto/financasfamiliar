050841e8e19fe9bf969cd65fcef64e5b212e487a
oliveira <leonidespires@gmail.com>
2022-08-02 14:41:02 -0300

    Initial commit

---
+++ src/main/java/com/desafiobackend/financasfamiliar/FinancasfamiliarApplication.java
@@ -0,0 +1,13 @@
+package com.desafiobackend.financasfamiliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasfamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasfamiliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiobackend/financasfamiliar/FinancasfamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiobackend.financasfamiliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasfamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
