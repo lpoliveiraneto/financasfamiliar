7ee7dd61accdba9b3157a4293649815c51ca4280
oliveira <leonidespires@gmail.com>
2022-08-16 22:15:59 -0300

    Criar rotas e services para listar todas despesas, buscar despesa por id e salvar despesa.

--- src/main/java/com/desafiobackend/financasfamiliar/api/controller/DespesaController.java
+++ src/main/java/com/desafiobackend/financasfamiliar/api/controller/DespesaController.java
@@ -1,7 +1,38 @@
 package com.desafiobackend.financasfamiliar.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.desafiobackend.financasfamiliar.domain.model.Despesa;
+import com.desafiobackend.financasfamiliar.domain.service.DespesaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DespesaController {
+
+    @Autowired
+    private DespesaService despesaService;
+
+    @GetMapping
+    public List<Despesa> retrieveAllDespesa(){
+        return despesaService.listaTodasDespesa();
+    }
+
+    @GetMapping("/{id}")
+    public Despesa retrieveDespesa(@PathVariable Integer id){
+        return despesaService.buscaDespesa(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Despesa> saveDespesa(@RequestBody Despesa despesa){
+
+        Despesa despesaSave = despesaService.salvarDespesa(despesa);
+        return new ResponseEntity<>(despesaSave, HttpStatus.CREATED);
+    }
+
+
+
+
 }
